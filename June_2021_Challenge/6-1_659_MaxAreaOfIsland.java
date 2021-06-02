fff1932abd41251033e9fb962f84a06e5b857a32
xixiz1024 <85067932+xixiz1024@users.noreply.github.com>
2021-06-02 00:33:10 -0400

    Update 6-1_659_MaxAreaOfIsland.java

--- June_2021_Challenge/6-1_659_MaxAreaOfIsland.java
+++ June_2021_Challenge/6-1_659_MaxAreaOfIsland.java
@@ -40,6 +40,8 @@ class Solution {
 
 
 // Example 1:
+<img src="Image/659.jpeg">
+
 // Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
 // Output: 6
 // Explanation: The answer is not 11, because the island must be connected 4-directionally.
